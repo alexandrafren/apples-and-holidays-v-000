c99b6dba333aafd279187c2f51e15e2ef182d0af
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-12 16:52:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,6 @@ def all_supplies_in_holidays(holiday_hash)
         puts "  #{holiday,join(" ")}: #{supplies.join(", ")}"
       end
    end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
