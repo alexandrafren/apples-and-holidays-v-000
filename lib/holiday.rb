0b2deb102a0d1ed10991348e8a90f130cf0dbf96
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-12 16:24:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
        holiday.each do |word|
          new_array << word.capitalize!
        end
-       
+       holiday = new_array.join(", ")
        puts "  #{holiday.to_s.split("_").join(" ").capitalize}: #{supply_array.join(", ")}"
      end
    end
